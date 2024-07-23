46082a9e58149599fe3d92c837358ed7df397f16
edwinnelly <112112833+edwinnelly@users.noreply.github.com>
2024-07-23 02:56:54 +0100

    ok
    index updated

---
+++ inc/header.php
@@ -0,0 +1,55 @@
+<div class="header header-dark">
+			<div class="container">
+				<nav id="navigation" class="navigation navigation-landscape">
+					<div class="nav-header">
+						<a class="nav-brand static-show" href="index.php"><img src="logo.png" style="width: 100px;" class="logo" alt=""></a>
+						<a class="nav-brand mob-show" href="index.php"><img src="logo.png" style="" class="logo" style="color: aqua;" alt=""></a>
+						<div ></div>
+                        <img class="nav-toggle" src="https://cdn3.iconfinder.com/data/icons/ui-controls-3/24/overflow_menu_vertical-512.png"  alt="">
+						<div class="mobile_nav">
+							<ul>
+								<!-- <li class="currencyDropdown me-2">
+									<a href="" class="nav-link" data-bs-toggle="modal" data-bs-target="#currencyModal"><span
+											class="fw-medium">NGN</span></a>
+								</li>
+								<li class="languageDropdown me-2">
+									<a href="" class="nav-link" data-bs-toggle="modal" data-bs-target="#countryModal"><img
+											src="assets/img/flag/flag.png" class="img-fluid" width="17" alt="Country"></a>
+								</li> -->
+								<li>
+									<!-- <a href="" class="bg-light-primary text-primary rounded" data-bs-toggle="modal"
+										data-bs-target="#login"><i class="fa-regular fa-circle-user fs-6"></i></a> -->
+                                        <img src="https://cdn4.iconfinder.com/data/icons/buno-users/32/user_account_login-512.png" height="40" alt="">
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="nav-menus-wrapper" style="transition-property: none;">
+						<ul class="nav-menu">
+
+							<li class="active"><a href="home-stay.html"><i class="fa-solid fa-umbrella-beach me-2"></i>Stays</a></li>
+							<li><a href="home-flight.html"><i class="fa-solid fa-jet-fighter me-2"></i>Flights</a></li>
+							<li><a href="home-hotel.html"><i class="fa-solid fa-spa me-2"></i>Hotels</a></li>
+							<li><a href="home-rental.html"><i class="fa-solid fa-house-circle-check me-2"></i>Rental</a></li>
+							<li><a href="cars.php"><i class="fa-solid fa-car me-2"></i>Cars</a></li>
+
+						</ul>
+
+						<ul class="nav-menu nav-menu-social align-to-right">
+							<li class="currencyDropdown me-2">
+								<a href="home-5.html#" class="nav-link" data-bs-toggle="modal" data-bs-target="#currencyModal"><span
+										class="fw-medium">NGN</span></a>
+							</li>
+							<li class="languageDropdown me-2">
+								<a href="home-5.html#" class="nav-link" data-bs-toggle="modal" data-bs-target="#countryModal"><img
+										src="https://cdn2.iconfinder.com/data/icons/flags_gosquared/64/Nigeria_flat.png" class="img-fluid" width="17" alt="Country"></a>
+							</li>
+							<li class="list-buttons light">
+								<a href="home-5.html#" data-bs-toggle="modal" data-bs-target="#login"><i
+										class="fa-regular fa-circle-user fs-6 me-2"></i>Login / Register</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
